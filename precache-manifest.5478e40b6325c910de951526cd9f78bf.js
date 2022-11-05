227d45538a385c8edcfce626dac3fe5640b4ff7e
Code Sandbox <hello@codesandbox.io>
2022-11-05 12:01:58 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.5478e40b6325c910de951526cd9f78bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "378c14f3cd9b52def5f5dc71641601b4",
+    "url": "https://hrodrigues-tmforum.github.io/csb-5715w3/index.html"
+  },
+  {
+    "revision": "154edd6540ac59327b24",
+    "url": "https://hrodrigues-tmforum.github.io/csb-5715w3/static/js/2.3f4b260c.chunk.js"
+  },
+  {
+    "revision": "95b02c74358f8b38c858893fedf4b750",
+    "url": "https://hrodrigues-tmforum.github.io/csb-5715w3/static/js/2.3f4b260c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7973544863a004e86d5",
+    "url": "https://hrodrigues-tmforum.github.io/csb-5715w3/static/js/main.2233a994.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "https://hrodrigues-tmforum.github.io/csb-5715w3/static/js/main.2233a994.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "977c44a653961bf94cb1",
+    "url": "https://hrodrigues-tmforum.github.io/csb-5715w3/static/js/runtime-main.39f23ba1.js"
+  }
+]);
